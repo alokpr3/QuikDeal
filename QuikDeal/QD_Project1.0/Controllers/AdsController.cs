dfaffde3aea3425bacc497afa4406847b7a77af2
Alok Gupta <alok.ar625@gmail.com>
2017-06-26 22:14:47 +0530

    Project Started

---
+++ QuikDeal/QD_Project1.0/Controllers/AdsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QD_Project1._0.Controllers
+{
+    public class AdsController : Controller
+    {
+        // GET: Ads
+        public ActionResult Index()
+        {
+            return View("PostAd");
+        }
+    }
+}
---
+++ QuikDeal/QD_Project1.0/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QD_Project1._0.Startup))]
+namespace QD_Project1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
